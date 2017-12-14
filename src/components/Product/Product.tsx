b82205df2449fb265519ff9e00bc776fc1f77a9d
Tomasz Drwięga <tomusdrw@gmail.com>
2017-12-14 22:58:14 +0100

    Togglable description.

--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -12,7 +12,38 @@ export const Product = ({ id, name, price, description }: ProductType) => (
   <div className="product">
     <img src={`https://xpla.org/ext/lorempixel/250/250/technics/${id}`} />
     <h3>{name}</h3>
-    <p>{description}</p>
-    <h4 className="price">${(price / 100).toFixed(2)} zł</h4>
+    <Description {...{description}} />
+    <h4 className="price">{(price / 100).toFixed(2)} zł</h4>
   </div>
 );
+
+class Description extends React.Component<{ description: string }, { expanded: boolean }> {
+
+  state = {
+    expanded: false
+  };
+
+  handleToggle = () => {
+    this.setState(state => ({ expanded: !state.expanded }));
+  }
+
+  render () {
+    const { description } = this.props;
+
+    if (this.state.expanded) {
+      return (
+        <p>
+          {description}
+          <a onClick={this.handleToggle}> less</a>
+        </p>
+      );
+    }
+
+    return (
+      <p>
+        {description.slice(0, 64)}
+        <a onClick={this.handleToggle}> more...</a>
+      </p>
+    );
+  }
+}
